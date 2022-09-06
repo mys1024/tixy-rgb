911c38006906da3c1b8b8f4ea39b5c41fb385b10
mys1024 <mys.1024@outlook.com>
2022-09-06 23:17:08 +0800

    feat: add author info in footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,13 +1,18 @@
 export default () => (
-  <footer font-mono text-gray>
+  <footer font-mono text-sm text-center text-gray space-y-1>
     <p>
       <span>Inspired by </span>
-      <a text-sky href="https://tin.ddiu.io">tin.ddiu.io</a>
-      <span> & </span>
       <a text-sky href="https://tixy.land">tixy.land</a>
+      <span> & </span>
+      <a text-sky href="https://tin.ddiu.io">tin.ddiu.io</a>
       <span> & My </span>
       <span text-red>R</span><span text-green>G</span><span text-blue>B</span>
       <span> keyboard</span>
+    </p>
+    <p>
+      <a text-sky href="https://mys.1024.cab">mys1024</a>
+      <span> / </span>
+      <a text-sky href="https://github.com/mys1024/tixy-rgba">tixy-rgba</a>
     </p>
   </footer>
 )
