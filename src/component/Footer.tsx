99e49966a9c7bf3ab320701fabaf4093296f5fe2
mys1024 <mys.1024@outlook.com>
2022-09-07 02:15:45 +0800

    feat: increase footer's opacity

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,26 +1,26 @@
 export default () => (
   <footer space-y-1 font-mono text="sm center">
     <p>
-      <span text-gray opacity-80>Inspired by </span>
-      <span text-sky opacity="80 hover:100" transition>
+      <span text-gray opacity-90>Inspired by </span>
+      <span text-sky opacity="90 hover:100" transition>
         <a href="https://tixy.land">tixy.land</a>
       </span>
-      <span text-gray opacity-80> & </span>
-      <span text-sky opacity="80 hover:100" transition>
+      <span text-gray opacity-90> & </span>
+      <span text-sky opacity="90 hover:100" transition>
         <a href="https://tin.ddiu.io">tin.ddiu.io</a>
       </span>
-      <span text-gray opacity-80> & my </span>
-      <span text-red opacity-80>R</span>
-      <span text-green opacity-80>G</span>
-      <span text-blue opacity-80>B</span>
-      <span text-gray opacity-80> backlight keyboard</span>
+      <span text-gray opacity-90> & my </span>
+      <span text-red opacity-90>R</span>
+      <span text-green opacity-90>G</span>
+      <span text-blue opacity-90>B</span>
+      <span text-gray opacity-90> backlight keyboard</span>
     </p>
     <p>
-      <span text-sky opacity="80 hover:100" transition>
+      <span text-sky opacity="90 hover:100" transition>
         <a href="https://mys.1024.cab">mys1024</a>
       </span>
-      <span text-gray opacity-80> / </span>
-      <span text-sky opacity="80 hover:100" transition>
+      <span text-gray opacity-90> / </span>
+      <span text-sky opacity="90 hover:100" transition>
         <a href="https://github.com/mys1024/tixy-rgba">tixy-rgba</a>
       </span>
     </p>
