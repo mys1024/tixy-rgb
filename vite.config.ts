7443342bee44dacb5c184b658e8e98fe26d06d1c
mys1024 <mys.1024@outlook.com>
2022-10-03 00:20:23 +0800

    chore: optimize

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,19 @@
-import path from 'path'
 import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
+import ResolveAlias from 'vite-plugin-easy-resolve-alias'
+import Solid from 'vite-plugin-solid'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
+    // https://github.com/mys1024/vite-plugin-easy-resolve-alias
+    ResolveAlias({ '~/': 'src/' }),
     // https://github.com/solidjs/vite-plugin-solid
-    solidPlugin(),
+    Solid(),
     // https://github.com/unocss/unocss
     Unocss(),
   ],
   // https://github.com/vitest-dev/vitest
   test: {
     include: ['test/**/*.test.ts'],
-  },
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
   },
 })
